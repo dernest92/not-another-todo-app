841d0d29465c3f3699f3a4a6c2fbd45099953269
dernest92 <41268963+dernest92@users.noreply.github.com>
2019-05-01 16:36:04 -0500

    changed from local host

--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://not-another-todo-api.herokuapp.com/",
   withCredentials: false,
   headers: {
     Accept: "application/json",
