aa73df350b779c2b714e9275064fb7419769bde5
dernest92 <41268963+dernest92@users.noreply.github.com>
2019-05-02 12:11:02 -0500

    removed day header

--- src/components/Week.vue
+++ src/components/Week.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div v-if="isFirstWeek" class="week-header">
+    <!-- <div v-if="isFirstWeek" class="week-header">
       <div
         v-for="(day, index) in days"
         class="day-header"
         :key="index"
       >{{ moment(day).format("ddd")}}</div>
-    </div>
+    </div>-->
     <div class="week-container">
       <Day class="day" v-for="(date, index) in days" :key="index" :date="date"/>
     </div>
@@ -53,17 +53,17 @@ export default {
   width: 100%;
 }
 
-.week-header {
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
+// .week-header {
+//   display: grid;
+//   grid-template-columns: repeat(7, 1fr);
 
-  .day-header {
-    background: #ccc;
-    color: #333;
-    padding: 2px 0;
-    font-weight: bold;
-  }
-}
+//   .day-header {
+//     background: #ccc;
+//     color: #333;
+//     padding: 2px 0;
+//     font-weight: bold;
+//   }
+// }
 
 .day-buttons {
   display: flex;
